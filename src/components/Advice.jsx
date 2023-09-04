0215b0c0be19a69398c16e2bb2e75f16a90fb2b5
Waleed_Ahmed <0100waleedahmed0100@gmail.com>
2023-09-04 15:39:24 +0500

    Fix file path to image

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
-import {useState } from "react";
+import { useState } from "react";
 
 const Advice = () => {
-  const [advice, setAdvice] = useState("Allah helps those, who help themselves.");
+  const [advice, setAdvice] = useState(
+    "Allah helps those, who help themselves."
+  );
   const [index, setIndex] = useState(786);
 
   const fetchAdvice = async () => {
@@ -21,22 +23,20 @@ const Advice = () => {
 
   return (
     <section className="relative w-[346px] h-[315px] sm:w-[542px] sm:h-[335px] bg-DarkGrayishBlue rounded-xl pt-8 pb-12 px-8 sm:p-16 sm:pt-12 text-center flex flex-col drop-shadow-2xl">
-      <h1 className="text-NeonGreen text-sm tracking-[4px] font-bold">ADVICE #{index}</h1>
+      <h1 className="text-NeonGreen text-sm tracking-[4px] font-bold">
+        ADVICE #{index}
+      </h1>
       <div className="grow relative">
-        <p className="w-full text-quote text-LightCyan font-bold absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 before:content-[]">{advice}</p>
+        <p className="w-full text-quote text-LightCyan font-bold absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 before:content-[]">
+          {advice}
+        </p>
       </div>
-      <div className="content-[url(./src/assets/pattern-divider-desktop.svg)] sm:content-[url(./src/assets/pattern-divider-mobile.svg)]">
-        {/* <img
-          src="./src/assets/pattern-divider-mobile.svg"
-          alt="Divider"
-          className="my-4 mx-auto"
-        /> */}
-      </div>
+      <div className="content-[url(/public/images/pattern-divider-desktop.svg)] sm:content-[url(/public/images/pattern-divider-mobile.svg)]"></div>
       <button
         onClick={fetchAdvice}
         className=" p-5 bg-NeonGreen rounded-full absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 duration-150 ease-in-out hover:opacity-90"
       >
-        <img src="../assets/icon-dice.svg" />
+        <img src="/images/icon-dice.svg" alt="Show Advice" />
       </button>
     </section>
   );
